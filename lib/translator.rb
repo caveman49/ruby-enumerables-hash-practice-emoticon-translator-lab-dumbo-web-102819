69d37a5f9f760bd35bd37b7397ddb5b5edfba303
Eric Cave <caveman4949@gmail.com>
2019-10-25 13:53:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,4 +1,6 @@
 # require modules here
+require 'pry'
+require 'yaml'
 
 def load_library
   # code goes here
@@ -9,23 +11,4 @@ def get_japanese_emoticon
 end
 
 def get_english_meaning
-  # code goes here
-end
-
-test = {
-  :angel => ["O:)", ":("],
-  :angry => [">:(", ":)"]
-  }
-
-
-
-def translator(test)
-  puts "Please enter the emoticon that you'd like translate"
-  input = gets.chomp
-  input2 = gets.downcase.chomp
-  test.each do |emotion, emoticons|
-    if emoticons.first == input
-      puts "Here is the Japanese version of your emoticon:   #{emoticons.last}"
-    end
-  end
-end
+  # code
